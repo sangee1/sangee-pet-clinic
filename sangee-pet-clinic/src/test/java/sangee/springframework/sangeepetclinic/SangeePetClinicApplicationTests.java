91f62a7b021cf0a32f260812a1e90c2eb0439d5c
Sangee <sangee@github.com>
2018-09-18 12:43:43 +0530

    Initial commit

---
+++ sangee-pet-clinic/src/test/java/sangee/springframework/sangeepetclinic/SangeePetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package sangee.springframework.sangeepetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SangeePetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

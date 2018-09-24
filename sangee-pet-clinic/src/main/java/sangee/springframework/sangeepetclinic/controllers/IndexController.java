ea0219b21207b57644e340995fbbfec6ca7b96fb
Sangee <sangee@github.com>
2018-09-24 11:20:13 +0530

    Added Index page controller.

---
+++ sangee-pet-clinic/src/main/java/sangee/springframework/sangeepetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package sangee.springframework.sangeepetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"","/","index","index.html"})
+	String index(){
+		return "index";
+	}
+
+}

433a900886ab50023b2bb0ac866afec03a7b6085
Sangee <sangee@github.com>
2018-09-29 22:21:13 +0530

    Moving controllers to pet-clinic-web

---
+++ sangee-pet-clinic/pet-clinic-web/src/main/java/sangee.springframework.sangeepetclinic/controllers/OwnerController.java
@@ -0,0 +1,29 @@
+package sangee.springframework.sangeepetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sangee.springframework.sangeepetclinic.services.OwnerService;
+
+@RequestMapping({"/owners"})
+@Controller
+public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
+	@RequestMapping({"","/","/index","/index.html"})
+	public String listOwners(Model model){
+		
+		model.addAttribute("owners", ownerService.findAll());
+		return "owners/index";
+	}
+
+}
---
+++ sangee-pet-clinic/pet-clinic-web/src/main/java/sangee.springframework.sangeepetclinic/controllers/VetController.java
@@ -0,0 +1,26 @@
+package sangee.springframework.sangeepetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sangee.springframework.sangeepetclinic.services.VetService;
+
+@Controller
+public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
+	public String listVets(Model model){
+		model.addAttribute("vets", vetService.findAll());
+		return "vets/index";
+	}
+	
+
+}

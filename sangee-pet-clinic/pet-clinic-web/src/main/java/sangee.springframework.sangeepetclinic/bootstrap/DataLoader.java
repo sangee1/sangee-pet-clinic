7ccc8f6c2a2288d85949daf7c12c004b8925f7cc
Sangee <sangee@github.com>
2018-09-29 22:24:33 +0530

    Moving bootstrap to pet-clinic-web

---
+++ sangee-pet-clinic/pet-clinic-web/src/main/java/sangee.springframework.sangeepetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,54 @@
+package bootstrap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import sangee.springframework.sangeepetclinic.model.Owner;
+import sangee.springframework.sangeepetclinic.model.Vet;
+import sangee.springframework.sangeepetclinic.services.OwnerService;
+import sangee.springframework.sangeepetclinic.services.VetService;
+import sangee.springframework.sangeepetclinic.services.map.OwnerServiceMap;
+import sangee.springframework.sangeepetclinic.services.map.VetServiceMap;
+
+@Component
+public class DataLoader implements CommandLineRunner{
+	
+	private final OwnerService ownerService;
+	private final VetService vetService;
+	
+	public DataLoader(OwnerService ownerService, VetService vetService) {
+		super();
+		this.ownerService = ownerService;
+		this.vetService = vetService;
+	}
+
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Owner owner1 = new Owner();
+		owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+       ownerService.save(owner1);
+       
+       Owner owner2 = new Owner();
+       owner2.setFirstName("Fiona");
+       owner2.setLastName("Glenanne");
+       ownerService.save(owner2);
+       System.out.println("Loaded Owners....");
+       Vet vet1 = new Vet();
+       vet1.setFirstName("Sam");
+       vet1.setLastName("Axe");
+      
+       vetService.save(vet1);
+
+       Vet vet2 = new Vet();
+       
+       vet2.setFirstName("Jessie");
+       vet2.setLastName("Porter");
+       
+       vetService.save(vet2);
+       System.out.println("Loaded Vets....");
+		
+	}
+
+}

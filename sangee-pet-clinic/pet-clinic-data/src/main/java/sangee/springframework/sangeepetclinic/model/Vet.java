aad8d5d859bc128d03205f34686d30d3116559d3
Sangee <sangee@github.com>
2018-10-01 12:10:17 +0530

    Added Speciality and updated Vet to have specialities

--- sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/model/Vet.java
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/model/Vet.java
@@ -1,6 +1,20 @@
 package sangee.springframework.sangeepetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+	
+	private Set<Speciality> specialities;
+
+	public Set<Speciality> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
+	
+	
 	
 	
 

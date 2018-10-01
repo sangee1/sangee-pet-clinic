37a093f3bedfd9d3b2360cf0bc1203883afcdf2a
Sangee <sangee@github.com>
2018-10-01 12:20:19 +0530

    Added contact info to Owner

--- sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/model/Owner.java
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/model/Owner.java
@@ -3,7 +3,35 @@ package sangee.springframework.sangeepetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+	private String adddress;
+	private String city;
+	private String telephone;
+	
 	private Set<Pet> pets;
+
+	public String getAdddress() {
+		return adddress;
+	}
+
+	public void setAdddress(String adddress) {
+		this.adddress = adddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
@@ -11,6 +39,9 @@ public class Owner extends Person {
 
 	public void setPets(Set<Pet> pets) {
 		this.pets = pets;
+	}
+
+	
 	}
 	
 	

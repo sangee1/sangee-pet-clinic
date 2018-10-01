b41f154bbcf9a6b770f340a520695e2ab2164571
Sangee <sangee@github.com>
2018-10-01 18:04:57 +0530

    Create SpecialityMapService

---
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package sangee.springframework.sangeepetclinic.services;
+
+import sangee.springframework.sangeepetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+
+}
---
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,44 @@
+package sangee.springframework.sangeepetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import sangee.springframework.sangeepetclinic.model.Speciality;
+import sangee.springframework.sangeepetclinic.services.SpecialitiesService;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService{
+
+	@Override
+	public Set<Speciality> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality o) {
+		// TODO Auto-generated method stub
+		return super.save(o);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality o) {
+		// TODO Auto-generated method stub
+		super.delete(o);
+	}
+	
+
+}

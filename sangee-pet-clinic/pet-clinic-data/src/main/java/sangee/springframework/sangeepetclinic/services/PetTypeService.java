6bebc3d7ab7975a7fa7fcb1e3ae5c3e8be652f38
Sangee <sangee@github.com>
2018-10-01 12:35:17 +0530

    Added PetTypeService and PetTypeMapService

---
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package sangee.springframework.sangeepetclinic.services;
+
+
+import sangee.springframework.sangeepetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+
+}
---
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,44 @@
+package sangee.springframework.sangeepetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import sangee.springframework.sangeepetclinic.model.PetType;
+import sangee.springframework.sangeepetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService{
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType o) {
+		// TODO Auto-generated method stub
+		return super.save(o);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType o) {
+		// TODO Auto-generated method stub
+		super.delete(o);
+	}
+	
+
+}

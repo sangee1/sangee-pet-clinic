4bd420df73a917f51bd7b42ae29c5ad106c237a9
Sangee <sangee@github.com>
2018-10-04 18:20:19 +0530

    Added VisitService and VisitMapService

---
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package sangee.springframework.sangeepetclinic.services;
+
+import sangee.springframework.sangeepetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
---
+++ sangee-pet-clinic/pet-clinic-data/src/main/java/sangee/springframework/sangeepetclinic/services/map/VisitMapService.java
@@ -0,0 +1,52 @@
+package sangee.springframework.sangeepetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import sangee.springframework.sangeepetclinic.model.Vet;
+import sangee.springframework.sangeepetclinic.model.Visit;
+import sangee.springframework.sangeepetclinic.services.VetService;
+import sangee.springframework.sangeepetclinic.services.VisitService;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+	@Override
+	public Set<Visit> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+		if(visit.getPet() == null || visit.getPet().getOwner() == null
+				|| visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null){
+			throw new RuntimeException("Invalid visit");
+		}
+		return super.save(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Visit o) {
+		// TODO Auto-generated method stub
+		super.delete(o);
+	}
+
+	
+		
+	
+
+}

8ef21b5a11c62494611646627177bf0e977a83fc
Sangee <sangee@github.com>
2021-12-08 10:57:48 +0530

    Initial commit for Spring Pet Clinic Project

---
+++ sangee-pet-clinic/src/main/java/com/example/sangeepetclinic/SangeePetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.sangeepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangeePetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangeePetClinicApplication.class, args);
+	}
+
+}
---
+++ sangee-pet-clinic/src/test/java/com/example/sangeepetclinic/SangeePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sangeepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangeePetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
